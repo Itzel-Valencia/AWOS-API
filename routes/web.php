9d72f732b1ad7c3f47efe8bc85a00323568c1c89
Valencia_Nava Itzel <al221910521@gmail.com>
2020-10-13 04:53:31 -0500

    practicas

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('Helloworld', function () {
+     return view('Helloworld');
+    
+    
+});
